4c69a4c09b057e9984ac293e002f24a74537dfd1
matlittle <matlittle727@gmail.com>
2017-12-26 01:12:23 -0600

    Added environment port variable for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
